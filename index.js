56d4527e8a5417474fe87670ed825cec67f1221f
tmhglnd <timohoogland@gmail.com>
2020-05-10 23:11:52 +0200

    added index.html

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
+const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log('Listening on port: ' + port));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
