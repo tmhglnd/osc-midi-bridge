ba9a1e4fd397aa8f73e40337613212390c28dc65
tmhglnd <timohoogland@gmail.com>
2020-05-11 23:40:34 +0200

    update console and random username

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-// const path = require('path');
 const express = require('express');
-// const app = express();
 const socket = require('socket.io');
 
 const PORT = process.env.PORT || 5000;
@@ -12,49 +10,20 @@ let server = express()
 .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
 .listen(PORT, () => console.log('Listening on port: ' + PORT));
 
-// app.use(express.static(path.join(__dirname, 'public')))
-// app.get('/', (req, res) => res.send('Hello World!'));
-// app.listen(port, () => console.log('Listening on port: ' + port));
-
 const io = socket(server);
-// const { Server } = require('ws');
-// const wss = new Server({ server });
 
-// wss.on('connection', (ws) => {
-// 	console.log('Client connected', ws);
-// 	ws.on('close', () => console.log('Client disconnected'));
-// });
 
 io.on('connection', (socket) => {
-	let username;
 	socket.on('user', (user) => {
-		username = user;
 		console.log('new user:', user, socket.id);
 	})
 	
 	socket.on('disconnect', () => {
-		console.log('user disconnected:', username, socket.id);
+		console.log('user disconnected:', socket.id);
 	});
 
 	socket.on('message', (msg) => {
 		console.log('received:', msg);
 		io.emit('message', msg);
 	});
-})
-
-// setInterval(() => {
-// 	wss.clients.forEach((client) => {
-// 	  client.send(new Date().toTimeString());
-// 	});
-// }, 1000);
-
-// setInterval(() => {
-// 	io.emit('message', new Date().toTimeString())
-// }, 1000);
-
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('pages/index'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+});
