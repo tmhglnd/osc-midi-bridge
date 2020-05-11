ea35bd247363d9ac50ec3a2abc91e98312105b40
tmhglnd <timohoogland@gmail.com>
2020-05-11 11:31:10 +0200

    added websocket

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log('Listening on port: ' + port));
 
+const { Server } = require('ws');
+const wss = new Server(app);
+
+wss.on('connection', (ws) => {
+	console.log('Client connected');
+	ws.on('close', () => console.log('Client disconnected'));
+});
+
+setInterval(() => {
+	wss.clients.forEach((client) => {
+	  client.send(new Date().toTimeString());
+	});
+}, 1000);
+
 // express()
 //   .use(express.static(path.join(__dirname, 'public')))
 //   .set('views', path.join(__dirname, 'views'))
